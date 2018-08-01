b9229dd46e9dc1aa8fc86c51b6edaa30e0b2b770
reardj3 <reardj3@USENREARDJ3L1C.corp.emc.com>
2018-08-01 16:15:23 -0400

    Added multiple brick colors depending on location

--- src/Brick.java
+++ src/Brick.java
@@ -11,9 +11,31 @@ public class Brick extends Sprite {
 		
 		this.setVisible(true);
 		
-		URL brickUrl = Brick.class.getResource("/Brick.png");
-		ImageIcon ii = new ImageIcon(brickUrl);
-		this.setImage(ii.getImage());
+		String color;		
+		if (y < 115) {
+			color = "Purple";
+		} else if (y < 230) {
+			color = "Blue";
+		} else if (y < 345) {
+			color = "Cyan";
+		} else if (y < 460) {
+			color = "Green";
+		} else if (y < 575) {
+			color = "Yellow";
+		} else if (y < 690) {
+			color = "Orange";
+		} else {
+			color = "Red";
+		}
+		
+		try {
+			URL brickUrl = Brick.class.getResource("/" + color + "Brick.png");
+			ImageIcon ii = new ImageIcon(brickUrl);
+			this.setImage(ii.getImage());
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+			System.out.println(color);
+		}
 		
 		this.setHeight(this.getImage().getHeight(null));
 		this.setWidth(this.getImage().getWidth(null));
--- src/Constants.java
+++ src/Constants.java
@@ -1,7 +1,7 @@
 
 public interface Constants {
 
-	public static final int WIDTH = 700;
+	public static final int WIDTH = 715;
 	public static final int HEIGHT = 1080;
 	public static final int PAD_X_START = 250;
 	public static final int PAD_Y_START = 1000;
